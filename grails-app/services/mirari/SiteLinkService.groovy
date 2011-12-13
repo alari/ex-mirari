11abcd7823212abdecf651b6d764b402f2c64ab3
Dmitry Kurinskiy <name.alari@gmail.com>
2011-12-13 19:54:35 +0400

    bugfix

--- grails-app/services/mirari/SiteLinkService.groovy
+++ grails-app/services/mirari/SiteLinkService.groovy
@@ -14,29 +14,32 @@ class SiteLinkService {
 
     LinkGenerator grailsLinkGenerator
 
+    @Typed
     String getUrl(Site site, Map args = [:]) {
         args.action = args.action ?: ""
         args.controller = args.controller ?: "site"
         args.params = args.params ?: [:]
-        args.params.siteName = site.name
+        ((Map)args.params).siteName = site.name
         grailsLinkGenerator.link(args)
     }
 
+    @Typed
     String getUrl(Page page, Map args = [absolute:true]) {
         args.action = args.action ?: ""
         args.controller = args.controller ?: "sitePage"
         args.params = args.params ?: [:]
-        args.params.siteName = ((Page)page).site.name.toString()
-        args.params.pageName = page.name ?: "null"
+        ((Map)args.params).siteName = ((Page)page).site.name.toString()
+        ((Map)args.params).pageName = page.name ?: "null"
         grailsLinkGenerator.link(args)
     }
 
+    @Typed
     String getUrl(Unit unit, Map args = [:]) {
         args.action = args.action ?: ""
         args.controller = args.controller ?: "siteUnit"
         args.params = args.params ?: [:]
-        args.params.siteName = unit.owner.name
-        args.params.id = unit.id.toString()
+        ((Map)args.params).siteName = unit.owner.name
+        ((Map)args.params).id = unit.id.toString()
         grailsLinkGenerator.link(args)
     }
 }
